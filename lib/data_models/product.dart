5653ec53cacb3b6dfd24f04f574b143a289bc0f1
Ali Amin <ali.m.amin98@gmail.com>
2019-08-02 12:49:11 +0200

    Add description to product model

--- lib/data_models/product.dart
+++ lib/data_models/product.dart
@@ -1,20 +1,27 @@
-import 'package:flutter/material.dart';
-
 class Product {
   String _name;
   String _price;
   String _photoUrl;
   int _backgroundColor = 0xFF2547F4;
+  String _description;
 
   String get name => _name;
   String get price => _price;
   String get photoUrl => _photoUrl;
   int get color => _backgroundColor;
+  String get description => _description;
 
-  Product({String name, String price, String photoUrl, int color}) {
+  Product({
+    String name,
+    String price,
+    String photoUrl,
+    int color,
+    String description,
+  }) {
     _name = name;
     _price = price;
     _photoUrl = photoUrl;
     _backgroundColor = color;
+    _description = description;
   }
 }
