2fa0fae6449a8a6d84fc98671b082ee9f0541e03
Ali Amin <ali.m.amin98@gmail.com>
2019-08-02 13:47:28 +0200

    Avoid navigation bar from moving with textfield

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatelessWidget {
       appBar: HomeAppBar(
         height: MediaQuery.of(context).size.height,
       ),
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Theme.of(context).primaryColor,
       body: Stack(
         children: <Widget>[
