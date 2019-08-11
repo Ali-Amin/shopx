e58e5ea615fe25b255476753274b488d08de4cba
Ali Amin <ali@Alis-MacBook-Pro.local>
2019-08-11 17:14:34 +0200

    animate product screen transition

--- lib/screens/product/widgets/product_info/product_info.dart
+++ lib/screens/product/widgets/product_info/product_info.dart
@@ -2,22 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:ShopX/screens/product/widgets/product_info/favourite_button.dart';
 import 'package:ShopX/screens/product/widgets/product_info/info_card.dart';
 
-class ProductInfo extends StatelessWidget {
+class ProductInfo extends StatefulWidget {
+  @override
+  _ProductInfoState createState() => _ProductInfoState();
+}
+
+class _ProductInfoState extends State<ProductInfo> {
+  double y = 250;
+  double opacity = 0.1;
+
+  Future animate() {
+    return Future.delayed(Duration(milliseconds: 50)).then(
+      (_) => setState(() {
+        y = 0;
+        opacity = 1;
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 400,
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: InfoCard(),
-          ),
-          Align(
-            alignment: Alignment(0.7, -1),
-            child: FavouriteButton(),
-          ),
-        ],
+    animate();
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 400),
+      curve: Curves.decelerate,
+      opacity: opacity,
+      child: AnimatedContainer(
+        curve: Curves.decelerate,
+        duration: Duration(milliseconds: 200),
+        transform: Matrix4.translationValues(0, y, 0),
+        height: 400,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: InfoCard(),
+            ),
+            Align(
+              alignment: Alignment(0.7, -1),
+              child: FavouriteButton(),
+            ),
+          ],
+        ),
       ),
     );
   }
