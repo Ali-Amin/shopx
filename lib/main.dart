1075e6a15dda94150766cdc4e238affea7e56b4f
Ali Amin <ali@Alis-MacBook-Pro.local>
2019-08-11 13:31:51 +0200

    Wrap material app with store

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shoptronics/screens/screens.dart';
+import 'package:shoptronics/stores/store.dart';
 import 'package:shoptronics/theme.dart';
 
 void main() => runApp(Shoptronics());
 
-class Shoptronics extends StatelessWidget {
+class Shoptronics extends StatefulWidget {
+  @override
+  _ShoptronicsState createState() => _ShoptronicsState();
+}
+
+class _ShoptronicsState extends State<Shoptronics> {
+  AppStore store;
+
+  @override
+  void initState() {
+    store = AppStore();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ShoptronicsTheme().themeData,
-      home: HomeScreen(),
+    return Provider<AppStore>.value(
+      value: store,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ShoptronicsTheme().themeData,
+        home: HomeScreen(),
+      ),
     );
   }
 }
