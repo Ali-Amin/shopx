468cc2ffc92625a6d7fc66726dbb436de7f30ca3
Ali Amin <ali@Alis-MacBook-Pro.local>
2019-08-11 15:59:36 +0200

    Animate list to inital position on category change

--- lib/screens/home/widgets/explore/explore.dart
+++ lib/screens/home/widgets/explore/explore.dart
@@ -29,21 +29,45 @@ class Explore extends StatelessWidget {
   }
 }
 
-class ExploreProductList extends StatelessWidget {
+class ExploreProductList extends StatefulWidget {
+  @override
+  _ExploreProductListState createState() => _ExploreProductListState();
+}
+
+class _ExploreProductListState extends State<ExploreProductList> {
+  ScrollController controller;
+  @override
+  void initState() {
+    controller = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final store = Provider.of<AppStore>(context);
-    return Observer(
-      builder: (context) => SizedBox(
+    return Observer(builder: (context) {
+      controller.animateTo(
+        0,
+        curve: Curves.decelerate,
+        duration: Duration(milliseconds: 200),
+      );
+      return SizedBox(
           height: 250,
           child: ListView.builder(
             padding: const EdgeInsets.only(left: 32.0),
             physics: BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
             itemCount: store.products.length,
+            controller: controller,
             itemBuilder: (context, index) => Provider<Product>.value(
                 value: store.products[index], child: ExploreProductCard()),
-          )),
-    );
+          ));
+    });
   }
 }
